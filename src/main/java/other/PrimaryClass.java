2e439568d7b805d2498bf37fc4d5f7af2e2e4e3a
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-10-17 17:53:06 -0300

    more duplicate code added

---
+++ src/main/java/other/PrimaryClass.java
@@ -0,0 +1,27 @@
+package other;
+
+public class PrimaryClass {
+
+    public void print() {
+        System.out.println("print1");
+        System.out.println("print2");
+        System.out.println("print3");
+        System.out.println("print4");
+        System.out.println("print5");
+        System.out.println("print6");
+        System.out.println("print7");
+        System.out.println("print8");
+        System.out.println("print9");
+        System.out.println("print10");
+        System.out.println("print11");
+        System.out.println("print12");
+        System.out.println("print13");
+        System.out.println("print14");
+        System.out.println("print15");
+        System.out.println("print16");
+        System.out.println("print17");
+        System.out.println("print18");
+        System.out.println("print19");
+        System.out.println("print20");
+    }
+}
