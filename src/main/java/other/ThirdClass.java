1a1a42288abc5e05d45f54fd78ffb647db8ed0d8
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-10-17 18:26:03 -0300

    third class

---
+++ src/main/java/other/ThirdClass.java
@@ -0,0 +1,13 @@
+package other;
+
+public class ThirdClass {
+
+    public void enumerate() {
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+    }
+}
