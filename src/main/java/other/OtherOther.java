ac20e78b20a910df64ff3c32d7b4a263f6814de0
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-10-18 23:37:30 -0300

    new class again

---
+++ src/main/java/other/OtherOther.java
@@ -0,0 +1,12 @@
+package other;
+
+public class OtherOther {
+    public void enumerate() {
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+        System.out.print(1);
+    }
+}
