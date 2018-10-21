db215a4b1258ec160af3d431222bbe3da9bfd441
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-10-21 20:26:44 -0300

    new class

--- src/main/java/guava/Fighterclass.java
+++ src/main/java/guava/Fighterclass.java
@@ -25,4 +25,19 @@ public class Fighterclass {
 
         table.put("row3", "col1", 51);
     }
+
+    public static void main2(String[] args) {
+        Table<String, String, Integer> table = HashBasedTable.create();
+
+        table.put("row1", "col1", 5);
+        table.put("row1", "col2", 6);
+        table.put("row1", "col3", 3);
+        table.put("row1", "col4", 2);
+
+        table.put("row2", "col1", 0);
+        table.put("row2", "col2", -1);
+        table.put("row2", "col3", 3);
+
+        table.put("row3", "col1", 51);
+    }
 }
